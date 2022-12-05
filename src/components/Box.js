cd8c71a66984e2dc5cf42f61336037ac9b4f1d0a
rayan <randomhex1998@gmail.com>
2022-12-05 22:14:31 +0330

    make-copy-function-and-add-react-toastify

--- src/components/Box.js
+++ src/components/Box.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { BASE_URL } from "../shared/BaseUrl";
 import { Btn } from "../shared/Btn";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Box = () => {
   const [qoute, setQuote] = useState("");
@@ -21,18 +23,36 @@ const Box = () => {
     });
   };
 
+  const copy = () => {
+    navigator.clipboard.writeText(qoute);
+    toast.success("Copied To clipboard ..!", {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+      style: { backgroundColor: "#2ecc71" },
+    });
+  };
+
   return (
-    <div className="main-box">
-      <h2>Generate Quotes</h2>
-      <div className="Content-box">
-        <p className="text-box">{qoute}</p>
-        <h4 className="author-box">{author}</h4>
+    <>
+      <div className="main-box">
+        <h2>Generate Quotes</h2>
+        <div className="Content-box">
+          <p className="text-box">{qoute}</p>
+          <h4 className="author-box">{author}</h4>
+        </div>
+        <div className="btn-area">
+          <Btn content="Generate" cls="btn gen-btn" clkFunc={apiHandler} />
+          <Btn content="Copy" cls="btn copy-btn" clkFunc={copy} />
+        </div>
       </div>
-      <div className="btn-area">
-        <Btn content="Generate" cls="btn gen-btn" clkFunc={apiHandler} />
-        <Btn content="Copy" cls="btn copy-btn" />
-      </div>
-    </div>
+      <ToastContainer />
+    </>
   );
 };
 
